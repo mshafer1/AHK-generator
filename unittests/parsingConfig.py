f48fa43be4bbb3cd07f8aac7e6d0d955adeb8644
Matthew-Shafer-Tower <mshafer1@harding.edu>
2017-09-23 09:22:09 -0500

    adding more config parse tests

--- unittests/parsingConfig.py
+++ unittests/parsingConfig.py
@@ -2,9 +2,8 @@ import unittest
 from baseTest import base
 import time
 
-page = '/?length=1&comment0=test+comment&func0=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue0=i&Window0=ahk_exe&Program0=chrome.exe&option0=ActivateOrOpen'
+page = '/?length=1&comment0=test+comment&func0=KEY&skey0%5B%5D=CTRL&skey0%5B%5D=ALT&skeyValue0=i&Window0=ahk_exe+chrome.exe&Program0=chrome.exe&option0=ActivateOrOpen'
 class TestComment(base):
-	
     @classmethod
     def setUpClass(cls):
         """ get_some_resource() is slow, to avoid calling it for each test use setUpClass()
@@ -37,10 +36,76 @@ class TestComment(base):
         comment = parsed_html.body.find('input', attrs={'id': 'comment0'})
         self.assertNotEquals(None, comment)
 
-        ### can't get comment text box value for some reason
-        #self.assertEqual("test comment", comment.get('value'))
+        comment_value = self.driver.find_element_by_id('comment0').get_attribute('value')
+        self.assertEqual("test comment", comment_value)
         # print htmlSource
-      
+
+    def test_options(self):
+        print "\tTest: has options"
+        options_to_check = {
+            'CTRL':True,
+            'ALT':True,
+            'WIN':False,
+            'SHIFT':False
+        }
+        htmlSource = self.driver.page_source
+
+        parsed_html = self.parser(htmlSource)
+        for key, expected in options_to_check.iteritems():
+            chbox = parsed_html.body.find('input', attrs={'id': 'skey0{0}'.format(key)})
+            print '\t\tchecking {0}'.format(key)
+            print '\t\texpecting {0}'.format(expected)
+            self.assertNotEquals(None, chbox)
+            chbox_value = self.driver.find_element_by_id('skey0{0}'.format(key)).is_selected()
+            self.assertEqual(expected, chbox_value)
+
+    def test_function(self):
+        print "\tTest: ActivateOrOpen selected"
+        htmlSource = self.driver.page_source
+
+        parsed_html = self.parser(htmlSource)
+        # print [x for x in dir(parsed_html.body.find('div', attrs={'id': 'shortcut0'})) if '_' not in x]
+        option = parsed_html.body.find('input', attrs={'id': 'option0'})
+        self.assertNotEquals(None, option)
+
+        option_value = self.driver.find_element_by_id('option0').get_attribute('value')
+        self.assertEqual("ActivateOrOpen", option_value)
+        # print htmlSource
+
+    def test_modes(self):
+        print "\tTest: hotkey mode"
+        options_to_check = {
+            'KEY': True,
+            'STRING': False
+        }
+
+        htmlSource = self.driver.page_source
+
+        parsed_html = self.parser(htmlSource)
+        for key, expected in options_to_check.iteritems():
+            rbox = parsed_html.body.find('input', attrs={'id': 'func0{0}'.format(key)})
+            print '\t\tchecking {0}'.format(key)
+            print '\t\texpecting {0}'.format(expected)
+            self.assertNotEquals(None, rbox)
+            rbox_value = self.driver.find_element_by_id('func0{0}'.format(key)).is_selected()
+            self.assertEqual(expected, rbox_value)
+
+    def test_function_inputs(self):
+        print "\tTest: has options"
+        options_to_check = {
+            'window0': 'ahk_exe chrome.exe',
+            'program0': 'chrome.exe'
+        }
+        htmlSource = self.driver.page_source
+
+        parsed_html = self.parser(htmlSource)
+        for key, expected in options_to_check.iteritems():
+            tx_box = parsed_html.body.find('input', attrs={'id': key})
+            print '\t\tchecking {0}'.format(key)
+            print '\t\texpecting {0}'.format(expected)
+            self.assertNotEquals(None, tx_box)
+            tx_box_value = self.driver.find_element_by_id(key).get_attribute('value')
+            self.assertEqual(expected, tx_box_value)
       
     def test_downloadEnabled(self):
         print "\tTest: Download disabled"
