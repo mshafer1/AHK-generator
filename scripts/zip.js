3c7425d889cfc7b00a379eaaf06440af80830f43
Matthew <2565361+mshafer1@users.noreply.github.com>
2020-09-13 20:36:02 -0500

    code cleaning

--- scripts/zip.js
+++ scripts/zip.js
@@ -5,16 +5,22 @@ try {
     // pass
 }
 
+const CURRENT_COMPRESSION_VERSION = "1.0"
+const DEFAULT_ENCODING = "Base64"
+
 function zip(string) {
-    var result = LZUTF8.compress(string,  {outputEncoding: 'Base64'} )
-    return `version=1.0&compressed=${result}`;
+    var result = LZUTF8.compress(string,  {outputEncoding: DEFAULT_ENCODING} )
+    return `version=${CURRENT_COMPRESSION_VERSION}&compressed=${result}`;
 }
 
 function unzip(compressed, version) {
     var result = ''
-    version = (version == undefined)? "1.0": version
+    version = (version == undefined)? CURRENT_COMPRESSION_VERSION: version
     if (version == "1.0") {
-        result = LZUTF8.decompress(compressed,  {inputEncoding: 'Base64'} );
+        result = LZUTF8.decompress(compressed,  {inputEncoding: DEFAULT_ENCODING} );
+    }
+    else {
+        throw 'Could not handle compression version: ' + version
     }
     return result
 }
